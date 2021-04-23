7e4790d0032c14c017860c22adc9bd9194d7579d
MeghanaKancherla <meghana.kancherla2911@gmail.com>
2021-04-23 10:59:29 +0530

    Android Project

---
+++ app/src/main/java/com/capgemini/androidapp/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.capgemini.androidapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    fun onClicking(view: View){
+        Toast.makeText(this, "Button  is clicked!", Toast.LENGTH_LONG).show()
+    }
+
+    val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called")
+
+        val b1 = findViewById<Button>(R.id.button2)
+
+        b1.setOnClickListener {
+            Toast.makeText(this, "Next button is pressed!", Toast.LENGTH_LONG).show()
+            val i: Intent = Intent(this, MainActivity2::class.java)
+            startActivity(i)
+        }
+        // One time execution
+        // Long running tasks shouldn't be put under onCreate() method
+        // respond within 5 sec - ANR (Application not responding) dialog
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        // save data - persistance
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // put data in bundle
+    }
+
+    var count = 0
+    override fun onBackPressed() {
+        count ++
+
+        if(count == 2){
+            super.onBackPressed()
+        }
+        else
+        {
+            Toast.makeText(this,"Press Back again to exit",Toast.LENGTH_LONG).show()
+        }
+    }
+}
+
+
